67e0f48df49dde98fa27652fe7910d54ba5afe59
robertlocskai <robertlocskai@gmail.com>
2023-11-13 17:07:06 +0100

    feat: added services container

--- client/src/views/ShopView.vue
+++ client/src/views/ShopView.vue
@@ -4,8 +4,17 @@
     <div class="fadeOut"></div>
   </div>
   <div class="container">
+    <div class="shopThumbnail">Teszt shopppp</div>
     <div class="content">
-      {{ $route.params.id }}
+      <!--{{ $route.params.id }}-->
+      <div class="row">
+        <div class="services col-7">
+          <div class="row">
+            <Service />
+          </div>
+        </div>
+        <div class="datas col-5"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -43,9 +52,33 @@
   background-color: white;
   transform: translateY(-48rem) translateX(-50%);
   border-radius: 9px;
+  overflow: hidden;
+}
+
+.shopThumbnail {
+  margin: -1rem -1rem 0rem -1rem;
+  height: 17rem;
+  background:
+    linear-gradient(0deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.45) 100%),
+    url('../assets/images/hero.png'),
+    lightgray 50% / cover no-repeat;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 4rem;
+  font-weight: 600;
 }
 
 .content {
   margin: 3rem;
+}
+
+.services {
+  border: green 1px solid;
+}
+.datas {
+  border: red 1px solid;
 }
 </style>
